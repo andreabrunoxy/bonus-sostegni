53373ab929ec2c0442765e0e90983d8f2ab38e45
andreabrunoxy <andreabrunoxy@users.noreply.github.com>
2021-04-01 10:19:09 +0200

    offset reduced

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ $("#runner").on("mouseover", function () {
   var offset = $(this).offset();
   var goX = Math.random() < 0.5 ? -1 : 1;
   var goY = Math.random() < 0.5 ? -1 : 1;
-  $(this).css("top", offset.top + 45 * goY);
-  $(this).css("left", offset.left + 120 * goX);
+  $(this).css("top", offset.top + 40 * goY);
+  $(this).css("left", offset.left + 80 * goX);
 });
